06c83715e58da9560dc14305875c45dd851c44e2
Allen Ellis <github@aell.is>
2023-05-24 19:37:38 -0700

    Included example usage

---
+++ README.md
@@ -0,0 +1,123 @@
+
+# Resolume Arena/Avenue JavaScript SDK
+
+An SDK to connect your TypeScript application with the REST API of the [Resolume Arena & Avenue](https://resolume.com) media server.
+
+
+
+## Overview
+
+Resolume Arena & Avenue is a live VJ & media server application for Mac and Windows. Versions 7.0 and up include an integrated REST API-compatible web server to allow for remote and automated control of the software while it is running.
+
+This SDK is a library of functions that are compatible with the [officially distributed schema](https://resolume.com/docs/restapi/swagger.yaml). Using this allows you to build software that can interact with this API.
+## Run Locally
+
+Clone the project
+
+```bash
+  git clone https://github.com/white-tie-live/resolume-js
+```
+
+Go to the project directory
+
+```bash
+  cd resolume-js
+```
+
+Install dependencies
+
+```bash
+  npm install
+```
+
+1. Either [download](https://resolume.com/download), install & launch Arena or Avenue, or have it running on a host accessible over your network.
+
+2. Make sure the Webserver is enabled from Area > Settings > Webserver.
+
+3. If needed, edit the host or port values in `index.ts`.
+
+4. Run the demo function:
+
+```bash
+  npm run demo
+```
+
+In your terminal, you should see a long JSON output representing your Arena composition. In addition, it adds a new column to the end of your current composition.
+
+
+## Example Usage
+
+Look inside the `ResolumeAPI` class inside `resolume.ts` to see all of the currently implemented API calls.
+
+To use them, follow the pattern shown in `index.ts`:
+
+First, establish a connection to the Arena/Avenue webserver:
+
+```tsx
+const hostValue = "127.0.0.1";
+const portValue = "8081";
+const pathValue = "/path/to/local/files";
+
+const resolume: ResolumeAPI = new ResolumeAPI(hostValue, +portValue, pathValue);
+
+let composition: Composition;
+try {
+    composition = await resolume.getComposition();
+} catch (error) {
+    console.log("Error connecting to Resolume:", error);
+    return;
+}
+```
+
+Now that you have this connection established, you can use the available functions. For example, adding a new column:
+
+```tsx
+await resolume.addColumn().catch((error) => {
+    console.log(`Couldn't add column: ${error}`);
+});
+```
+
+Or to import a specific graphic to a clip based on its index (column 1, layer 1):
+
+```tsx
+const path = "clip1.mov"
+const layerIndex = 1;
+const columnIndex = 1;
+
+await resolume.openClipByIndex(
+    layerIndex,
+    columnIndex,
+    path,
+    "file"
+).catch((error) => {
+    console.log(`Couldn't open file at ${path}: ${error}`);
+})
+ ```
+
+There are many endpoints available in the REST API, but only the basic ones currently have functions implemented through this SDK. You can add your own by following the established patterns in `resolume.ts`.
+## References
+
+- [Resolume Arena REST API Overview](https://resolume.com/support/en/restapi)
+- [API Documentation](https://resolume.com/docs/restapi/)
+
+
+
+## Used By
+
+This project is used by the following projects:
+
+[**Script Elephant**](https://scriptelephant.com): A web-based scripting and graphics cueing platform.
+
+
+
+## Support
+
+This is an unofficial package maintained by enthusiastic users of Resolume Arena.
+
+For support, join the [official Resolume Slack](https://join.slack.com/t/resolumecommunity/shared_invite/zt-kv27u7xj-TGwE~kZBTHKkyh169sbHrg) and join the **#rest-api** channel.
+
+
+## Authors
+
+- [Allen Ellis](https://www.github.com/allenellis)
+- [White Tie Productions](https://whitetielive.com)
---
+++ resolume.ts
@@ -0,0 +1,157 @@
+import {components} from "./schema";
+
+type APIResponse = components["schemas"]["ProductInfo"];
+type Deck = components["schemas"]["Deck"];
+type Composition = components["schemas"]["Composition"];
+type Column = components["schemas"]["Column"];
+type Clip = components["schemas"]["Clip"];
+type TransportTimeline = components["schemas"]["TransportTimeline"];
+type Layer = components["schemas"]["Layer"];
+type ProductInfo = components["schemas"]["ProductInfo"];
+type VideoEffect = components["schemas"]["VideoEffect"];
+type VideoTrackClip = components["schemas"]["VideoTrackClip"];
+
+type StringParameter = components["schemas"]["StringParameter"];
+type ColorParameter = components["schemas"]["ColorParameter"];
+type ChoiceParameter = components["schemas"]["ChoiceParameter"];
+type ParameterCollection = components["schemas"]["ParameterCollection"];
+type TextParameter = components["schemas"]["TextParameter"];
+
+
+export class ResolumeAPI {
+    host: string;
+    port: number;
+    filePath?: string;
+    protocol?: string;
+
+
+    constructor(host: string, port: number, filePath?: string, protocol?: string) {
+        this.host = host;
+        this.port = port;
+        this.filePath = filePath || "";
+        this.protocol = protocol || "http";
+    }
+
+    url() {
+        return `${this.protocol}://${this.host}:${this.port}/api/v1`;
+    }
+
+    static errorOpeningClip = class extends Error {
+        constructor(message: string) {
+            super(message);
+            this.name = "errorOpeningClip";
+        }
+    }
+
+    async getProduct(): Promise<ProductInfo> {
+        return await fetch(this.url() + `/product`).then((response) => response.json())
+    }
+
+    async getComposition(): Promise<Composition> {
+        return await fetch(this.url() + `/composition`).then((response) => response.json())
+    }
+
+
+    async putDeck(deck: Deck): Promise<any> {
+        return await fetch(
+            this.url() + `/composition/decks/by-id/${deck.id}`,
+            {method: 'PUT', body: JSON.stringify(deck)}
+        ).then((response) => response)
+    }
+
+    async addColumn(): Promise<any> {
+        return await fetch(
+            this.url() + `/composition/columns/add`,
+            {method: 'POST'}
+        ).then((response) => response)
+    }
+
+    async replaceColumn(index: number, column: Column): Promise<any> {
+        return await fetch(
+            this.url() + `/composition/columns/${index}`,
+            {method: 'PUT', body: JSON.stringify(column)}
+        ).then((response) => response)
+    }
+
+    async addLayer(): Promise<any> {
+        return await fetch(
+            this.url() + `/composition/layers/add`,
+            {method: 'POST'}
+        ).then((response) => response)
+    }
+
+    async replaceLayer(index: number, layer: Layer): Promise<any> {
+        return await fetch(
+            this.url() + `/composition/layers/${index}`,
+            {method: 'PUT', body: JSON.stringify(layer)}
+        ).then((response) => response)
+    }
+
+    async getClipByIndex(layerIndex: number, clipIndex: number): Promise<Clip> {
+        console.log("Trying URL:", this.url() + `/composition/layers/${layerIndex}/clips/${clipIndex}`);
+        return await fetch(
+            this.url() + `/composition/layers/${layerIndex}/clips/${clipIndex}`,
+            {method: 'GET'}
+        ).then((response) => response.json())
+    }
+
+    async replaceClip(layerIndex: number, clipIndex: number, clip: Clip): Promise<any> {
+        return await fetch(
+            this.url() + `/composition/layers/${layerIndex}/clips/${clipIndex}`,
+            {method: 'PUT', body: JSON.stringify(clip)}
+        ).then((response) => response)
+    }
+
+    async addVideoEffect(layerIndex: number, clipIndex: number, effectName: string): Promise<any> {
+        return await fetch(
+            this.url() + `/composition/layers/${layerIndex}/clips/${clipIndex}/effects/video/add`,
+            {method: 'POST', body: "effect:///video/" + encodeURI(effectName)}
+        ).then((response) => response)
+    }
+
+    async addVideoSource(layerIndex: number, clipIndex: number, effectName: string): Promise<any> {
+        return await fetch(
+            this.url() + `/composition/layers/${layerIndex}/clips/${clipIndex}/open`,
+            {method: 'POST', body: "source:///video/" + encodeURI(effectName)}
+        ).then((response) => response)
+    }
+
+
+    async openClipByIndex(
+        layerIndex: number,
+        clipIndex: number,
+        path: string,
+        encoding: "file" | "source" | "raw" = "file"
+    ): Promise<any> {
+        let data: string
+        if (encoding === "file") {
+            data = "file://" + encodeURI(path);
+        } else if (encoding === "source") {
+            data = "source://" + encodeURI(path);
+        } else {
+            data = path;
+        }
+
+        console.log("Trying URL", this.url() + `/composition/layers/${layerIndex}/clips/${clipIndex}/open`)
+
+        // @ts-ignore
+        const response: Response | undefined = await Promise.race([
+            fetch(
+                this.url() + `/composition/layers/${layerIndex}/clips/${clipIndex}/open`,
+                {method: 'POST', body: data}
+            ),
+            new Promise((_, reject) =>
+                setTimeout(() => reject(new ResolumeAPI.errorOpeningClip("Timeout after 5000ms")), 5000)
+            ),
+        ]);
+
+        if (!response) {
+            throw new ResolumeAPI.errorOpeningClip("Response not received");
+        }
+
+        if (response.status >= 300) {
+            throw new ResolumeAPI.errorOpeningClip(response.statusText);
+        }
+    }
+
+}
