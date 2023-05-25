0370c30ed260750e7321275ef60786830bf9b798
Allen Ellis <github@aell.is>
2023-05-24 19:37:38 -0700

    Included example usage

--- index.ts
+++ index.ts
@@ -1,1 +1,46 @@
-console.log("Hello world 6");
+import {ResolumeAPI} from "./resolume";
+import {components} from "./schema";
+
+type Composition = components["schemas"]["Composition"];
+
+async function interactWithResolume() {
+
+    const hostValue = "127.0.0.1";
+    const portValue = "8081";
+    const pathValue = "/path/to/local/files";
+
+    const resolume: ResolumeAPI = new ResolumeAPI(hostValue, +portValue, pathValue);
+
+    let composition: Composition;
+    try {
+        composition = await resolume.getComposition();
+    } catch (error) {
+        console.log("Error connecting to Resolume:", error);
+        return;
+    }
+    console.log("Composition is", composition);
+
+    // Add a column
+    await resolume.addColumn().catch((error) => {
+        console.log(`Couldn't add column: ${error}`);
+    });
+
+    // Import a file into a specific clip
+    const path = "clip1.mov"
+    const layerIndex = 1;
+    const columnIndex = 1;
+
+    await resolume.openClipByIndex(
+        layerIndex,
+        columnIndex,
+        path,
+        "file"
+    ).catch((error) => {
+        console.log(`Couldn't open file at ${path}: ${error}`);
+    })
+
+}
+
+interactWithResolume().then(() => {
+    console.log("Done!");
+})
